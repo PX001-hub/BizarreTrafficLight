eddddde40d1ea31b6cfd524dc4a676f074a54cf1
Pttr <draivv@yandex.ru>
2022-10-19 13:34:37 +0300

    Initial Commit

---
+++ BizarreTrafficLight/BizarreTrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  BizarreTrafficLightApp.swift
+//  BizarreTrafficLight
+//
+//  Created by P Z on 19/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct BizarreTrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
